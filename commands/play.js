f9bee6e33b983720a1df508a787ea7260fbc968a
7CO <hoangmen9918@gmail.com>
2025-04-25 17:59:23 +0700

    Fixed my dumb query mistake

--- commands/play.js
+++ commands/play.js
@@ -1,20 +1,19 @@
 
 
 function play(client, message, args) {
+    const query = args.join(" ");
 
-    const query = args.shift();
     const voice = message.member.voice.channel;
     if (!voice)
         return message.reply('Vui lòng vào voice để bắt đầu phát nhạc!');
     if (!query)
         return message.reply('Vui lòng điền tên bài hát hoặc link');
     try {
-        let result = client.distube.play(voice, query, {
+        client.distube.play(voice, query, {
             textChannel: message.channel,
             member: message.member,
             position: 0,
         });
-        console.log(result);
     } catch (error) {
         console.log(error);
         message.reply('Lỗi khi phát nhạc!');
