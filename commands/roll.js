7e1ddaa8c492dd6fc87eef70b5937fb8c2553ec1
DKpoosh <poosh0803@gmail.com>
2025-06-16 20:57:17 +1000

    back to v1

--- commands/roll.js
+++ commands/roll.js
@@ -7,36 +7,36 @@ const outcomes = [
     { name: '陰筊', image: '../images/roll/11.png' },
 ];
 
-// async function roll(message) {
-//     const randomOutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
-//     const imagePath = path.join(__dirname, randomOutcome.image);
-//     const attachment = new AttachmentBuilder(imagePath);
-
-//     await message.channel.send({
-//         content: `🎲 ${message.author} 擲筊: **${randomOutcome.name}**`,
-//         files: [attachment],
-//     });
-// };
-
 async function roll(message) {
     const randomOutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
     const imagePath = path.join(__dirname, randomOutcome.image);
     const attachment = new AttachmentBuilder(imagePath);
 
-    const embed = new EmbedBuilder()
-        .setColor(0x00AE86)
-        .setAuthor({
-            name: `${message.author.username} 擲筊`,
-            iconURL: message.author.displayAvatarURL(),
-        })
-        .setTitle(`🎲 結果: **${randomOutcome.name}**`) // Bold formatting works here
-        .setImage(`attachment://${path.basename(imagePath)}`)
-        .setTimestamp();
-
     await message.channel.send({
-        embeds: [embed],
+        content: `🎲 ${message.author} 擲筊: **${randomOutcome.name}**`,
         files: [attachment],
     });
-}
+};
+
+// async function roll(message) {
+//     const randomOutcome = outcomes[Math.floor(Math.random() * outcomes.length)];
+//     const imagePath = path.join(__dirname, randomOutcome.image);
+//     const attachment = new AttachmentBuilder(imagePath);
+
+//     const embed = new EmbedBuilder()
+//         .setColor(0x00AE86)
+//         .setAuthor({
+//             name: `${message.author.username} 擲筊`,
+//             iconURL: message.author.displayAvatarURL(),
+//         })
+//         .setTitle(`🎲 結果: **${randomOutcome.name}**`) // Bold formatting works here
+//         .setImage(`attachment://${path.basename(imagePath)}`)
+//         .setTimestamp();
+
+//     await message.channel.send({
+//         embeds: [embed],
+//         files: [attachment],
+//     });
+// }
 
 module.exports = roll;
